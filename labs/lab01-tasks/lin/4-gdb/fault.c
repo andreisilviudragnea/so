4128b6b49c621348a11faeb8bf55fc73c75de605
Andrei-Silviu DRAGNEA (24887) <andrei.dragnea@stud.acs.upb.ro>
2018-03-05 18:16:27 +0200

    Solved 4-gdb

--- labs/lab01-tasks/lin/4-gdb/fault.c
+++ labs/lab01-tasks/lin/4-gdb/fault.c
@@ -7,45 +7,43 @@
  * Debugging with gdb
  */
 
-
 #include <stdio.h>
 
-#define ARRAY_MAX_SIZE 8192
-
-void print_array(int v[], int n)
+static void print_array(int v[], int n)
 {
-	int i;
+    int i;
 
-	for (i = 0; i < ARRAY_MAX_SIZE; i++) {
-		printf("%d ", v[i]);
-		if (i % 11 == 0)
-			printf("\n");
-	}
+    for (i = 0; i < n; i++) {
+        if (i % 11 == 0)
+            printf("\n");
+        printf("%d ", v[i]);
+    }
 }
 
-int inv_sum(int v[], int n)
+static float inv_sum(int v[], int n)
 {
-	int i, isum = 0;
+    int i;
+    float isum = 0;
 
-	for (i = 0; i < n; i++)
-		isum += 1/v[i];
-	return isum;
+    for (i = 0; i < n; i++)
+        isum += 1.0 / v[i];
+    return isum;
 }
 
-void show_inv(int v[], int n)
+static void show_inv(int v[], int n)
 {
-	printf("Inverses sum for: [ ");
+    printf("Inverses sum for: [ ");
 
-	print_array(v, n);
+    print_array(v, n);
 
-	printf("] is: %d\n", inv_sum(v, n));
+    printf("\n] is: %g\n", inv_sum(v, n));
 }
 
 int main(void)
 {
-	int v[] = { 4, 3, 0, 3, 2};
+    int v[] = { 4, 3, 3, 2 };
 
-	show_inv(v, 5);
+    show_inv(v, 4);
 
-	return 0;
+    return 0;
 }
