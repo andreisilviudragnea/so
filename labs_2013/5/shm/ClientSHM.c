39d32cc4f2775155bc7afdc8f2e0d4fc86f7bc08
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2018-03-11 23:15:50 +0200

    Shared memory example

---
+++ labs_2013/5/shm/ClientSHM.c
@@ -0,0 +1,37 @@
+#include <windows.h>
+#include "utils.h"
+
+#define BUF_SIZE 256
+LPSTR szMapName = "MyFileMappingObject";
+
+int main(void)
+{
+    HANDLE hMapFile;
+    LPVOID lpMapAddress;
+    BOOL bRet;
+
+    hMapFile = OpenFileMapping(
+        FILE_MAP_ALL_ACCESS,                 /* read/write access */
+        FALSE,                               /* do not inherit the name */
+        szMapName);                          /* name of mapping object */
+    DIE(hMapFile == NULL, "CreateFileMapping");
+
+    lpMapAddress = MapViewOfFile(
+        hMapFile,        /* handle to map object */
+        FILE_MAP_ALL_ACCESS,    /* read/write permission */
+        0,            /* offset (high-order) */
+        0,            /* offset (low-order) */
+        0);
+    DIE(lpMapAddress == NULL, "MapViewOfFile");
+
+    printf("Message in shm is: <%s>\n", (char *) lpMapAddress);
+    fflush(stdout);
+
+    bRet = UnmapViewOfFile(lpMapAddress);
+    DIE(bRet == FALSE, "UnmapViewOfFile");
+
+    bRet = CloseHandle(hMapFile);
+    DIE(bRet == FALSE, "CloseHandle");
+
+    return 0;
+}
---
+++ labs_2013/5/shm/ServerSHM.c
@@ -0,0 +1,44 @@
+#include <windows.h>
+#include "utils.h"
+
+#define BUF_SIZE 256
+LPSTR szMapName = "MyFileMappingObject";
+LPSTR szMsg = "Testing shared memory on windows";
+
+
+int main(void)
+{
+    HANDLE hMapFile;
+    LPVOID lpMapAddress;
+    BOOL bRet;
+
+    hMapFile = CreateFileMapping(
+        INVALID_HANDLE_VALUE,         /* use swap, not a particular file */
+        NULL,                      /* default security */
+        PAGE_READWRITE,                  /* read/write access */
+        0,                          /* maximum object size (high-order DWORD) */
+        1024,                      /* maximum object size (low-order DWORD) */
+        szMapName);                  /* name of mapping object */
+    DIE(hMapFile == NULL, "CreateFileMapping");
+
+    lpMapAddress = MapViewOfFile(
+        hMapFile,          /* handle to map object */
+        FILE_MAP_ALL_ACCESS,  /* read/write permission */
+        0,              /* offset (high-order) */
+        0,              /* offset (low-order) */
+        0);
+    DIE(lpMapAddress == NULL, "MapViewOfFile");
+
+    ZeroMemory(lpMapAddress, strlen(szMsg) + 1);
+    CopyMemory(lpMapAddress, szMsg, strlen(szMsg));
+
+    Sleep(5000);
+
+    bRet = UnmapViewOfFile(lpMapAddress);
+    DIE(bRet == FALSE, "UnmapViewOfFile");
+
+    bRet = CloseHandle(hMapFile);
+    DIE(bRet == FALSE, "CloseHandle");
+
+    return 0;
+}
